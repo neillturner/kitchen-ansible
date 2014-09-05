760079eb99acb800f4bf44156d9c6fe26afdc22d
James Cuzella <james.cuzella@lyraphase.com>
2014-09-05 15:37:40 -0600

    Surround RSpec tests with "describe" blocks as in test-kitchen's spec
    Examples:
    
     - [chef_base_spec](https://github.com/test-kitchen/test-kitchen/blob/master/spec/kitchen/provisioner/chef_base_spec.rb)
    
     - [chef_solo_spec](https://github.com/test-kitchen/test-kitchen/blob/master/spec/kitchen/provisioner/chef_solo_spec.rb)
    
     - [chef_zero_spec](https://github.com/test-kitchen/test-kitchen/blob/master/spec/kitchen/provisioner/chef_zero_spec.rb)

--- spec/kitchen/provisioner/ansible_playbook_spec.rb
+++ spec/kitchen/provisioner/ansible_playbook_spec.rb
@@ -31,47 +31,51 @@ describe Kitchen::Provisioner::AnsiblePlaybook do
     {}
   end
 
-  it "should give a sane run_command" do
-    expect(provisioner.run_command).to match(/ansible-playbook/)
-  end
-
-  it "should not be verbose by default" do
-    expect(provisioner[:ansible_verbose]).to eq false
-    expect(provisioner.send(:ansible_verbose_flag)).to be_nil
-  end
-
-  it "should be configured to be verbose when ansible_verbose is set" do
-    config[:ansible_verbose] = true
-    expect(provisioner[:ansible_verbose]).to eq true
-  end
-
-  it "should generate the flag for 4 verbosity levels" do
-    config[:ansible_verbose] = true
-    (1..4).each do |i|
-      config[:ansible_verbosity] = i
-      # puts "Setting ansible_verbosity to: #{config[:ansible_verbosity]}"
-      # puts "ansible_verbose_flag is: #{provisioner.send(:ansible_verbose_flag)}"
-      expect( provisioner.send(:ansible_verbose_flag).count('v') ).to eq i
+  describe "#run_command" do
+    it "should give a sane run_command" do
+      expect(provisioner.run_command).to match(/ansible-playbook/)
     end
   end
 
-  it "should understand log level names and convert to a number of -v flags" do
-    config[:ansible_verbose] = true
-    { :info => 1, :warn => 2, :debug => 3, :trace => 4, 'info' => 1, 'warn' => 2, 'debug' => 3, 'trace' => 4 }.each do |log_level, i|
-      # puts "Setting ansible_verbosity to: #{log_level} which converts to integer: #{i}"
-      # puts "ansible_verbose_flag is: #{provisioner.send(:ansible_verbose_flag)}"
-      config[:ansible_verbosity] = log_level
-      expect{ provisioner.send(:ansible_verbose_flag) }.to_not raise_error
-      expect( provisioner.send(:ansible_verbose_flag).count('v') ).to eq i
+  describe "configuration" do
+    it "should not be verbose by default" do
+      expect(provisioner[:ansible_verbose]).to eq false
+      expect(provisioner.send(:ansible_verbose_flag)).to be_nil
     end
-  end
 
-  it "should raise an error if invalid verbosity level is given" do
-    config[:ansible_verbose] = true
-    [ 1e10, 0, -1, '-', :foobar, 'abc', {:foo => 'bar'}, [1,2,3] ].each do |invalid_level|
-      # puts "Setting ansible_verbosity to: #{invalid_level} which should raise error"
-      config[:ansible_verbosity] = invalid_level
-      expect{ provisioner.send(:ansible_verbose_flag) }.to raise_error
+    it "should be configured to be verbose when ansible_verbose is set" do
+      config[:ansible_verbose] = true
+      expect(provisioner[:ansible_verbose]).to eq true
+    end
+
+    it "should generate the flag for 4 verbosity levels" do
+      config[:ansible_verbose] = true
+      (1..4).each do |i|
+        config[:ansible_verbosity] = i
+        # puts "Setting ansible_verbosity to: #{config[:ansible_verbosity]}"
+        # puts "ansible_verbose_flag is: #{provisioner.send(:ansible_verbose_flag)}"
+        expect( provisioner.send(:ansible_verbose_flag).count('v') ).to eq i
+      end
+    end
+
+    it "should understand log level names and convert to a number of -v flags" do
+      config[:ansible_verbose] = true
+      { :info => 1, :warn => 2, :debug => 3, :trace => 4, 'info' => 1, 'warn' => 2, 'debug' => 3, 'trace' => 4 }.each do |log_level, i|
+        # puts "Setting ansible_verbosity to: #{log_level} which converts to integer: #{i}"
+        # puts "ansible_verbose_flag is: #{provisioner.send(:ansible_verbose_flag)}"
+        config[:ansible_verbosity] = log_level
+        expect{ provisioner.send(:ansible_verbose_flag) }.to_not raise_error
+        expect( provisioner.send(:ansible_verbose_flag).count('v') ).to eq i
+      end
+    end
+
+    it "should raise an error if invalid verbosity level is given" do
+      config[:ansible_verbose] = true
+      [ 1e10, 0, -1, '-', :foobar, 'abc', {:foo => 'bar'}, [1,2,3] ].each do |invalid_level|
+        # puts "Setting ansible_verbosity to: #{invalid_level} which should raise error"
+        config[:ansible_verbosity] = invalid_level
+        expect{ provisioner.send(:ansible_verbose_flag) }.to raise_error
+      end
     end
   end
 end
