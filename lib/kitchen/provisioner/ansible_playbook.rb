d1319c5048265d99a86e5ff8ce2387002752f1de
Pedro Salgado <steenzout@saucelabs.com>
2016-06-02 10:14:00 -0600

    upgrade setuptools so that kitchen-ansible can install ansible 2.x versions

--- lib/kitchen/provisioner/ansible_playbook.rb
+++ lib/kitchen/provisioner/ansible_playbook.rb
@@ -440,6 +440,7 @@ module Kitchen
           #{export_http_proxy}
           git clone git://github.com/ansible/ansible.git --recursive #{config[:root_path]}/ansible #{install_source_rev}
           #{sudo_env('easy_install')} pip
+          #{sudo_env('pip')} install -U setuptools
           #{sudo_env('pip')} install six paramiko PyYAML Jinja2 httplib2
         fi
         INSTALL
@@ -468,6 +469,7 @@ module Kitchen
 
           #{export_http_proxy}
           #{sudo_env('easy_install')} pip
+          #{sudo_env('pip')} install -U setuptools
           #{sudo_env('pip')} install ansible#{ansible_version}
         fi
         INSTALL
